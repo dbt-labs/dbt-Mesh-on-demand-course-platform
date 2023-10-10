df12013f3a6d47817c96e11f1bce21b972681ea7
Carol Ohms <carol.ohms@fishtownanalytics.com>
2023-10-10 03:28:34 +0000

    test 2

--- models/marts/fct_orders.sql
+++ models/marts/fct_orders.sql
@@ -14,7 +14,7 @@ final as (
         customer_name,
         location_name,
         tax_rate,
-        cast(location_opened_at as date) as location_opened_at,
+        location_opened_at,
         date_part(month, ordered_at) as ordered_month,
         date_part(day, ordered_at) as ordered_day, 
         date_part(year, ordered_at) as ordered_year
